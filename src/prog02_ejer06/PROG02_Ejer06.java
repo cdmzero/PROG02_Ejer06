4aab5a4c8e7f8ee614e81afe19e7a1509c076740
cdmzero <jose.funez.clase@gmail.com>
2021-11-09 13:03:56 +0100

    First Commit

---
+++ src/prog02_ejer06/PROG02_Ejer06.java
@@ -0,0 +1,26 @@
+
+/**
+ *
+ * @author zero
+ */
+public class PROG02_Ejer06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        // TODO code application logic here
+        
+    public enum meses{ MARZO }
+ 
+
+    // El método
+    public static void main(String[] args)
+    {
+        meses m = meses.MARZO;
+        System.out.println(m);
+    }
+        
+       
+    
+}
